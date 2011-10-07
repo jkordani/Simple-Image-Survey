d656f96573fdcc085869883c237f6a13439a846a
Joshua Kordani <joshua.kordani@gmail.com>
2011-10-07 16:39:00 -0400

    Added a config var page and a survey start template

---
+++ start.php
@@ -0,0 +1,24 @@
+<?php
+
+	#check for session state
+	#if state is blank redirect to index
+	
+	#if state is start
+		#check for submission of age and gender
+			#if true 
+			     #submit to database
+			     #fetch last id
+			     #set session user id
+			     #initialize survey numbering stack/list/array
+			     #set state to number of first quiz
+			     #redirect here
+			#else redirect to index
+
+	#if state is a number
+		#display image survey form using state number
+
+
+	#submit to this form?
+
+
+?>
