5250f9dded132d56283af7ffc4740d671d794643
Joshua Kordani <joshua.kordani@gmail.com>
2011-11-03 23:40:47 -0400

    Changed helpers to reflect desire for sequential image presentation

--- helpers.php
+++ helpers.php
@@ -1,15 +1,15 @@
 <?php
 function init_survey($images_directory){
-	 #if ordering file exists, read it in and return it
-	 if(file_exists('ordering')){
-		$ordering_fh = fopen("ordering","r");
-		$ordering_sstring = fgets($ordering_fh);
-		fclose($ordering_fh);
-		return unserialize($ordering_sstring);
-
-	 }
-	 #else read in a list of files from the array, mod it, store it
-	 else{
+	# #if ordering file exists, read it in and return it
+	# if(file_exists('ordering')){
+	#	$ordering_fh = fopen("ordering","r");
+	#	$ordering_sstring = fgets($ordering_fh);
+	#	fclose($ordering_fh);
+	#	return unserialize($ordering_sstring);
+	#
+	# }
+	##else read in a list of files from the array, mod it, store it
+	#else{
 		$ordering_array = scandir($images_directory);
 
 		#do this twice to get rid of . and .. from listing.
@@ -22,13 +22,13 @@ function init_survey($images_directory){
 			   unset($ordering_array[$thumbs]);
 		}
 
-		shuffle($ordering_array);
-		$ordering_fh = fopen("ordering","w+");
-		$ordering_sstring = serialize($ordering_array);
-		fputs($ordering_fh, $ordering_sstring);
-		fclose($ordering_fh);
-	 	return $ordering_array;
-	 }
+	#	shuffle($ordering_array);
+	#	$ordering_fh = fopen("ordering","w+");
+	#	$ordering_sstring = serialize($ordering_array);
+	#	fputs($ordering_fh, $ordering_sstring);
+	#	fclose($ordering_fh);
+		return $ordering_array;
+	#}
 
 }
 
