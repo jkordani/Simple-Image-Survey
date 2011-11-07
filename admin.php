0768fd18417b8fb79b0b65273f59ac751aa3accf
Joshua Kordani <joshua.kordani@gmail.com>
2011-11-06 19:40:18 -0500

    Added convenience list and auto set

--- admin.php
+++ admin.php
@@ -4,18 +4,30 @@ require_once('helpers.php');
 if(isset($_POST['survey_title'])){
    set_string_to_file($_POST['survey_title'],$g_current_survey_file);
 }
+
 $current_survey = get_string_from_file($g_current_survey_file);
+if(!isset($current_survey) || $current_survey == ''){
+        $temp_survey_array = $g_survey_array;
+	$current_survey = array_shift($temp_survey_array);
+	set_string_to_file($current_survey, $g_current_survey_file);
+}
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <title>Administration</title>
 </head>
 <body>
-Pick the name of the survey here.  In the future there will be a drop down of already enumerated.
+Pick the name of the survey here.
 <form id="generate-survey" method="post" action="admin.php">
- <input type="text" name="survey_title" />
+ <select name="survey_title">
+    <?php foreach($g_survey_array as $survey_dir){
+	      echo "<option>" . $survey_dir . "</option>\n";
+	  }
+    ?>
+ </select>
  <input type="submit" value="Set" />
 </form>
-The current survey is <?php echo ($current_survey == '') ? "blank" : $current_survey;?>
+The current survey is <?php echo $current_survey;?>
 </body>
 </html>
